3f214cccd63b87d7767854cf6d3508694d374346
Wingman <ryuash123@gmail.com>
2022-06-08 10:20:16 -0400

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
+  basePath: '/cosmoshub-staging',
   poweredByHeader: false,
   webpack: (config) => {
     config.module.rules.push({
